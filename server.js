f94d202a5b88c32d8bf5ee32d2808228d3628f73
YagnikAkbari2003 <yagnik.a@medkart.in>
2024-04-05 16:10:36 +0530

    change fallback port to 8080

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
@@ -12,29 +11,22 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
 app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
 
   try {
-    // Check if user exists
     const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(401).json({ error: "Invalid email or password" });
     }
 
-    // Check if password is correct
     if (password !== user.password) {
       return res.status(401).json({ error: "Invalid email or password" });
     }
-
-    // At this point, authentication is successful
-    // You can generate a token or session and send it back to the client
 
     res.status(200).json({
       message: "Login successful",
@@ -58,8 +50,11 @@ app.post("/api/logout", async (req, res) => {
 app.post("/api/subscribe", async (req, res) => {
   try {
     const { email } = req.body;
+
     const subscriber = new Subscriber({ email });
+
     await subscriber.save();
+
     res.status(201).json({ message: "Subscribed successfully!" });
   } catch (error) {
     console.error("Error subscribing:", error);
@@ -73,8 +68,8 @@ app.post("/api/unsubscribe", async (req, res) => {
   const { email } = req.body;
 
   try {
-    // Find the subscriber by email and remove them from the database
     await Subscriber.findOneAndDelete({ email });
+
     res.status(200).json({ message: "Unsubscribed successfully" });
   } catch (error) {
     console.error("Error unsubscribing:", error);
@@ -86,10 +81,8 @@ app.post("/api/unsubscribe", async (req, res) => {
 
 app.get("/api/subscribers", async (req, res) => {
   try {
-    // Query the database to get all subscriber data
-    const subscribers = await Subscriber.find({}); // Exclude _id field and only include email field
+    const subscribers = await Subscriber.find({});
 
-    // Return the subscriber data
     res.status(200).json(subscribers);
   } catch (error) {
     console.error("Error retrieving subscribers:", error);
@@ -101,10 +94,8 @@ app.get("/api/subscribers", async (req, res) => {
 
 app.get("/api/contacted", async (req, res) => {
   try {
-    // Query the database to get all contact us data
     const contactUsMessages = await ContactUs.find({});
 
-    // Return the contact us data
     res.status(200).json(contactUsMessages);
   } catch (error) {
     console.error("Error retrieving contact us messages:", error);
@@ -124,20 +115,18 @@ app.post("/api/send-email", async (req, res) => {
       });
     }
 
-    // Fetch all subscribers from the database
     const subscribers = await Subscriber.find();
 
     const OAuth2 = google.auth.OAuth2;
     const oauth2Client = new OAuth2(
       process.env.GOOGLE_AUTH_CLIENT_ID,
       process.env.GOOGLE_AUTH_CLIENT_SECRET,
-      process.env.GOOGLE_AUTH_REDIRECT_URL // Redirect URL
+      process.env.GOOGLE_AUTH_REDIRECT_URL
     );
     oauth2Client.setCredentials({
       refresh_token: process.env.GOOGLE_AUTH_REFRESH_TOKEN,
     });
 
-    // Create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
@@ -150,13 +139,12 @@ app.post("/api/send-email", async (req, res) => {
       },
     });
 
-    // Send email to each subscriber
     for (const subscriber of subscribers) {
       await transporter.sendMail({
-        from: "Growwitup Agency <your_email@gmail.com>", // Sender address
-        to: subscriber.email, // Subscriber's email
-        subject: title, // Subject line
-        html: `<p>${description}</p><a href="${process.env.URL}/unsubscribe">Unsubscribe</a>`, // Email body
+        from: `Growwitup Agency ${process.env.OWNER_EMAIL}`,
+        to: subscriber.email,
+        subject: title,
+        html: `<p>${description}</p><a href="${process.env.URL}/unsubscribe">Unsubscribe</a>`,
       });
     }
 
@@ -173,7 +161,6 @@ app.post("/api/send-email", async (req, res) => {
 
 app.get("/api/count", async (req, res) => {
   try {
-    // Query the database to get the total count of contact us submissions
     const totalContactUsCount = await ContactUs.countDocuments();
     const totalSubscribers = await Subscriber.countDocuments();
 
@@ -195,15 +182,13 @@ app.post("/api/contact-us", async (req, res) => {
     const oauth2Client = new OAuth2(
       process.env.GOOGLE_AUTH_CLIENT_ID,
       process.env.GOOGLE_AUTH_CLIENT_SECRET,
-      process.env.GOOGLE_AUTH_REDIRECT_URL // Redirect URL
+      process.env.GOOGLE_AUTH_REDIRECT_URL
     );
 
-    // Set refresh token
     oauth2Client.setCredentials({
       refresh_token: process.env.GOOGLE_AUTH_REFRESH_TOKEN,
     });
 
-    // Create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
@@ -216,18 +201,16 @@ app.post("/api/contact-us", async (req, res) => {
       },
     });
 
-    // Send mail with defined transport object
     await transporter.sendMail({
-      from: `Growwitup Agency ${email}`, // Sender address
-      // from: `"Growwitup Agency" ${email}`, // Sender address
-      to: `${process.env.OWNER_EMAIL}`, // Client's email
-      subject: `${name} Want to Contact.`, // Subject line
+      from: `Growwitup Agency ${email}`,
+      to: `${process.env.OWNER_EMAIL}`,
+      subject: `${name} Want to Contact.`,
       html: `
                 <h1>Name: ${name}</h1>
                 <p>Email: ${email}</p>
                 <p>Message: ${message}</p>
                 <hr>                
-            `, // HTML body
+            `,
     });
 
     res.status(200).json({ message: "Email sent successfully!" });
@@ -239,20 +222,17 @@ app.post("/api/contact-us", async (req, res) => {
   }
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 mongoose
   .connect(`${process.env.MONGO_URI}`)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`, {
-        useNewUrlParser: true,
-      });
+      console.log(`Server running on port ${PORT}`);
     });
     console.log("Database is Connected.");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Error connecting to database:", err);
     console.log("Database not connected!");
   });
