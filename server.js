ffec5b37d4dbe28d775ca94bc973b437ab7ce775
YagnikAkbari2003 <yagnik.a@medkart.in>
2024-04-05 16:49:19 +0530

    add home page

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to backend");
+});
+
 app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
 
